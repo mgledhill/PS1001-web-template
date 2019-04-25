7424a1ec27d36e11d4e58a549b4d424c3e1f5dcc
mgledhill <git@practicalseries.com>
2019-04-25 15:44:44 +0100

    P21: Published
    Section 14 added

--- 5-2361-website-source/01-pages/15-07-figures/05-local-js/15-07-scroll.js
+++ 5-2361-website-source/01-pages/15-07-figures/05-local-js/15-07-scroll.js
@@ -49,6 +49,8 @@ This is a complete summary of all software modifications.
 
 Date          Issue        Author         Reason for Modification
 -------------------------------------------------------------------------------
+25 Apr 2019   P21          M. Gledhill    Published
+
 25 Apr 2019   d-P20.15.01  M. Gledhill    Page created
 ---------------------------------------------------------------------------- */
 
@@ -68,7 +70,7 @@ $(document).ready(function() {                  /* START OF PAGE READY FUNCTION 
    **************************************************************************** */
 
     $('.aside-right-rev').append (
-        "<p>scroll.js&nbsp;&mdash;&nbsp;P20.15.01</p>" /* LOCAL JS REVISION NUMBER */
+        "<p>scroll.js&nbsp;&mdash;&nbsp;P21</p>" /* LOCAL JS REVISION NUMBER */
     );
 
 
